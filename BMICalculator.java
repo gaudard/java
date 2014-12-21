573e3d55f81f7d24844ceea86fc539470d491fb8
gaudard <gaudard@gmail.com>
2014-12-21 17:40:39 -0600

    complete

--- BMICalculator.java
+++ BMICalculator.java
@@ -2,4 +2,19 @@ import java.util.Scanner;
 
 public class BMICalculator
 {
-	
+	public static void main( String[] args )
+	{
+		Scanner kb = new Scanner(System.in);
+		double m, kg, bmi;
+		
+		System.out.print( "Your height in meters: " );
+		m = kb.nextDouble();
+		
+		System.out.print( "Your weight in kilograms: " );
+		kg = kb.nextDouble();
+		
+		bmi = kg / (m*m);
+		
+		System.out.println( "Your BMI is " + bmi );
+	}
+}
